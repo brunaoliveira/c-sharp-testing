80cd33c3f4403d20387e8eafada31f73662735b6
Bruna Oliveira <Bruna_Oliveira1@Dellteam.com>
2021-11-07 10:54:01 -0300

    Unit test return array

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -60,15 +61,21 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
-        public void GetOddNumbers_()
+        public void GetOddNumbers_LimitIsGreaterThanZero_ShouldReturnOddNumbersUpToLimit()
         {
-
             //Act
-
+            var result = _math.GetOddNumbers(5);
 
             //Assert
+            Assert.That(result, Is.Not.Empty);
 
+            Assert.That(result.Count(), Is.EqualTo(3));
+
+            Assert.That(result, Does.Contain(1));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo( new [] {1, 3, 5 } ));
         }
-
     }
 }
