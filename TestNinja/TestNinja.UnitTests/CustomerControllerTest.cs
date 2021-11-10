ba9cebf1602e30be8247b7298fe1d14407538d11
Bruna Oliveira <Bruna_Oliveira1@Dellteam.com>
2021-11-10 08:22:26 -0300

    Adding CustommerController unit tests

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTest
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ShouldReturnNotFound()
+        {
+            // Act
+            var result = _customerController.GetCustomer(0);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<NotFound>()); // resultado é exatamente do tipo NotFound
+            Assert.That(result, Is.InstanceOf<NotFound>()); // resultado do tipo NotFound ou um dos seus derivados
+        }
+    }
+}
